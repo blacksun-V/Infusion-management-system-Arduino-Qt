61e1ebdc16719b1bf659a76f889ee527e136a627
Mr.Wang <44333296+blacksun-V@users.noreply.github.com>
2020-05-24 13:51:06 +0800

    Add files via upload

---
+++ MCU/MCU.ino
@@ -0,0 +1,76 @@
+#define buzzer speakerPin
+#include <Servo.h>
+int speakerPin = 2;
+int length = 15; // the number of notes
+char notes[] = "ccggaagffeeddc "; // a space represents a rest
+int beats[] = { 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 4 };
+int tempo = 300;
+Servo myServo;
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(speakerPin, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(speakerPin, LOW);
+    delayMicroseconds(tone);
+  }
+}
+  
+void playNote(char note, int duration) {
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+  
+  // play the tone corresponding to the note name
+  for (int i = 0; i < 8; i++) {
+    if (names[i] == note) {
+      playTone(tones[i], duration);
+    }
+  }
+}
+
+void setup() {
+      // put your setup code here, to run once:
+    Serial.begin(9600);
+    pinMode(buzzer,OUTPUT);
+    digitalWrite(buzzer,LOW);
+    myServo.attach(3);
+    myServo.write(60);
+    pinMode(21,INPUT_PULLUP);
+    attachInterrupt(2,interrput,FALLING);
+}
+
+void loop() {
+  //sing();
+  // put your main code here, to run repeatedly:
+    if(Serial.available())
+    {
+         char Flag=Serial.read();
+         Serial.flush();
+         Serial.println(Flag);
+         if(Flag=='E')
+         {
+                for(int i=4;i>0;i--)
+                sing();
+          }
+          myServo.write(0);
+    }
+}
+
+void sing()
+{
+    for (int i = 0; i < length; i++) {
+        if (notes[i] == ' ') {
+          delay(beats[i] * tempo); // rest
+        } else {
+          playNote(notes[i], beats[i] * tempo);
+        }
+        // pause between notes
+        delay(tempo / 2);
+        myServo.write(0);
+    }
+}
+void(* resetFunc) (void) = 0; 
+void interrput()
+{
+  Serial.println("中断函数");
+  resetFunc();
+}
---
+++ WIFI/WIFI.ino
@@ -0,0 +1,89 @@
+#include <ESP8266WiFi.h>  //导入库
+#include <WiFiClient.h>
+#define AP_SSID "TP-LINK_DA5E" //这里改成你的wifi名字
+#define AP_PSW "feihu2016*."//这里改成你的wifi密码
+#define GPIO 2
+
+WiFiClient myClient;
+const char* server="192.168.1.107";
+String ip = "www.baidu.com";
+const int port=8080;
+int mybegin=0;
+
+void setup() { 
+    Serial.begin(9600);
+    //Serial.println("hhhhhh");
+    WiFi.mode(WIFI_STA);     //设置模式为STA
+    delay(2000);
+    WiFi.begin(AP_SSID, AP_PSW);  //启动STA模式，并连接wifi网络
+    ////判断网络状态是否连接上，没连接上就延时500ms，并且打出一个点，模拟连接过程
+    while (WiFi.status() != WL_CONNECTED){   //判断wifi是否连接成功
+        delay(500);
+    }
+    pinMode(GPIO,INPUT);
+}
+
+ 
+void loop() { 
+      //Serial.println("I am loop");
+      if(myClient.connected()==0)
+      {
+          mybegin=1;//说明连接断了
+          while(myClient.connect(server,port) != 1)
+          {
+             //myClient.stop();
+             //Serial.println("I am connecting!");
+          }
+      }
+      
+      if(mybegin==1)
+      {
+          myClient.print("{\"hwStatus\":\"begin\",\"clinical\":\"hw\",\"flag\":\"aa\"}");
+          mybegin=0;
+      }
+      bool s=1;
+      if((s=check())==0)//如果有水
+      {
+          int i=20;
+          while(myClient.connected()&&i>0)
+          {
+              s=check();
+              if(s != false)
+              {
+                  myClient.print("{\"hwStatus\":\"end\",\"clinical\":\"hw\",\"flag\":\"aa\"}");
+                  Serial.print("E"); 
+                  i--;
+               }
+              delay(8000);
+          }
+      }
+       
+      delay(10000);
+      //myClient.stop();
+ }
+
+ 
+ bool check()
+ {
+      int T=100;
+      bool st=0;
+      int sumture=0;
+      int sumfalse=0;
+      while(T>0)
+      {
+          st = digitalRead(GPIO);
+          if(st)
+          {
+              sumture++; 
+          }
+          else
+          {
+              sumfalse++; 
+          }
+          T--; 
+      }
+      if(sumture>sumfalse)
+      {return 1;}
+      else
+      {return 0;}
+ }
